672f93d325dbade0eac9a4cfe8f8b3e730039b7a
Ryan Shah <ryan.k.shah@gmail.com>
2018-03-03 16:12:06 +0000

    Removed Telnet functionality for now

--- backend/quick.py
+++ backend/quick.py
@@ -6,41 +6,9 @@ import socket
 import json
 import time
 
-RESULT_TRUE = 0
 BRUTEFORCE_TIMEOUT = 3
 # A list of root account username/password combinations for IoT devices
 IOT_ROOT_USER_COMBINATIONS = ('admin1', 'password'), ('root', 'xc3511'), ('root', 'vizxv'), ('root', 'admin'), ('admin', 'admin'), ('root', '888888'), ('root', 'xmhdipc'), ('root', 'default'), ('root', 'juantech'), ('root', '123456'), ('root', '54321'), ('support', 'support'), ('root', ''), ('admin', 'password'), ('root', 'root'), ('root', '12345'), ('user', 'user'), ('admin', '(none)'), ('root', 'pass'), ('admin', 'admin1234'), ('root', '1111'), ('admin', 'smcadmin'), ('admin', '1111'), ('root', '666666'), ('root', 'password'), ('root', '1234'), ('root', 'klv123'), ('Administrator', 'admin'), ('service', 'service'), ('supervisor', 'supervisor'), ('guest', 'guest'), ('guest', '12345'), ('guest', '12345'), ('admin1', 'password'), ('administrator', '1234'), ('666666', '666666'), ('888888', '888888'), ('ubnt', 'ubnt'), ('root', 'klv1234'), ('root', 'Zte521'), ('root', 'hi3518'), ('root', 'jvbzd'), ('root', 'anko'), ('root', 'zlxx.'), ('root', '7ujMko0vizxv'), ('root', '7ujMko0admin'), ('root', 'system'), ('root', 'ikwb'), ('root', 'dreambox'), ('root', 'user'), ('root', 'realtek'), ('root', '00000000'), ('admin', '1111111'), ('admin', '1234'), ('admin', '12345'), ('admin', '54321'), ('admin', '123456'), ('admin', '7ujMko0admin'), ('admin', '1234'), ('admin', 'pass'), ('admin', 'meinsm'), ('tech', 'tech'), ('mother', 'fucker')
-
-# Scan Redirect Function
-# =======================
-# Arguments
-#   - address: IP address
-#   - cidr: CIDR prefix to specify address range
-#   - mode: Scanning mode (SSH, Telnet or both)
-#   - cp: Change password on device and notify of change?
-#   - nd: Notify device of vulnerability?
-#
-# Description
-#   - Direct the command-line arguments to the
-#     appropriate scanning utility
-def scan(address, cidr, mode, cp, nd):
-    # Check if SSH, Telnet or both (otherwise fail)
-    if mode == 0:
-        # SSH
-        # append start
-        r = scan_ssh(address, cidr, cp, nd)
-        return json.dumps(r, sort_keys=True, indent=4)
-    elif mode == 1:
-        # Telnet
-        r = scan_telnet(address, cidr, cp, nd)
-        return json.dumps(r, sort_keys=True, indent=4)
-    elif mode == 2:
-        # Both
-        # TODO: Multithread SSH and Telnet?
-        return None
-    else:
-        # Fail
-        return None
 
 # SSH Scanning Function
 # ======================
@@ -55,6 +23,8 @@ def scan(address, cidr, mode, cp, nd):
 #     the root accounts found on the IP addresses found using the default
 #     list of username/password combinations defined as `passwords`, using
 #     an SSH connection
+#
+# TODO: Fix request timeout errors
 def scan_ssh(address, cidr, cp, nd):
     # A list of triples to identify a vulnerable device was found
     # in the form of (hostname, ip, mode)
@@ -82,11 +52,10 @@ def scan_ssh(address, cidr, cp, nd):
                 device.connect(str(ip), username=acc[0], password=acc[1], port=22, timeout=BRUTEFORCE_TIMEOUT)
 
                 # Check if notify device option was checked
+                # TODO: Fix notifying device
                 if nd == 1:
-                    device.exec_command('tput bel')
-                    device.exec_command('say "motherfucker"')
-                    #device.exec_command('')
-                    #device.exec_command("notify-send 'Miraihilate Alert!' 'A vulnerability has been detected in your device, linked with Mirai malware.'")
+                    # device.exec_command('')
+                    device.exec_command("notify-send 'Miraihilate Alert!' 'A vulnerability has been detected in your device, linked with Mirai malware.'")
 
                 # Returns a triple (hostname, aliaslist, ipaddrlist)
                 #   - See python documentation for socket
@@ -110,49 +79,7 @@ def scan_ssh(address, cidr, cp, nd):
     # or if nothing was found then return empty JSON
     return vulnerables
 
-# Telnet Scanning Function
-# ======================
-# Arguments
-#   - address: IP address
-#   - cidr: CIDR prefix to specify address range
-#   - cp: Change password on device and notify of change?
-#   - nd: Notify device of vulnerability?
-#
-# Description
-#   - Scans a specified network range and attempts to bruteforce into
-#     the root accounts found on the IP addresses found using the default
-#     list of username/password combinations defined as `passwords`, using
-#     a Telnet connection
-#
-# TODO: Finish Telnet scanning
-def scan_telnet(address, cidr, cp, nd):
-    # A list of triples to identify a vulnerable device was found
-    # in the form of (hostname, ip, mode)
-    vulnerables = []
-
-    for ip in IPNetwork(address + '/' + cidr):
-        print('Trying Telnet for IP "' + str(ip) + '" ...')
-
-        # Attempt to open a Telnet connection
-        try:
-            tnet = Telnet(str(ip), 2282, BRUTEFORCE_TIMEOUT)
-        except Exception as e:
-            # Try next IP if Telnet connection failed
-            print('\tFailed to connect with Telnet\n')
-            continue # Keep trying other passwords
-
-        # The connection has been established!
-        print('* Telnet was successful! *')
-
-        # TODO: Try username and password combinations to access device
-        # for acc in IOT_ROOT_USER_COMBINATIONS:
-
-        break
-
-    return vulnerables
-
 # TODO: Get command line arguments
 # TODO: Pass to scan function to run the appropriate scan
 
 # print('Vulnerables: ' + str(scan_ssh('127.0.0.1', '24', 0, 1)))
-print('\nVulnerables: ' + str(scan_telnet('127.0.0.1', '24', 0, 1)))
